5afb098dff35fa51edf28bb923b63de850c615ac
GaneshChaturthiCJ <67580297+GaneshChaturthiCJ@users.noreply.github.com>
2023-07-14 11:59:30 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,24 +1,21 @@
+const fs = require("fs");
 const express = require("express");
 const app = express();
-const products = require("./data/products.json");
+const products = JSON.parse(fs.readFileSync(`${__dirname}/data/products.json`));
 
 app.get("/api/v1/products", (req, res) => {
-  if (products.length > 0) {
-    res.status(200).json({
-      status: "success",
-      message: "Product fetched successfully",
-      data: {
-        products: products,
-      },
-    });
-  } else {
-    res.status(404).json({ message: "Product not found" });
+  if (products.length === 0) {
+    return res.status(404).json({ message: "Product not found" });
   }
+  res.status(200).json({
+    status: "success",
+    message: "Product fetched successfully",
+    data: {
+      products
+    }
+  });
 });
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+module.exports = app;
 
 
 
